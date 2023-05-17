ac3f22fcb9ee23ec05ca3135d24b501d7ec02bb8
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2023-05-16 22:26:37 -0700

    Api call testing

--- ViewModels/MainWindowViewModel.vb
+++ ViewModels/MainWindowViewModel.vb
@@ -1,6 +1,27 @@
-﻿Namespace ViewModels
+﻿Imports System.Net.Http
+Imports System.Text
+
+Namespace ViewModels
 
     Public Class MainWindowViewModel
+
+        Public Async Function PostDataAsync() As Task(Of String)
+            Dim client As New HttpClient()
+            Dim apiUrl As String = "http://127.0.0.1:8000/api/geolocation"
+
+            Dim requestBody As String = "{ ""ip_address"": ""73.157.229.121"" }"
+            Dim content As New StringContent(requestBody, Encoding.UTF8, "application/json")
+
+            Dim response As HttpResponseMessage = Await client.PostAsync(apiUrl, content)
+
+            If response.IsSuccessStatusCode Then
+                Dim responseContent As String = Await response.Content.ReadAsStringAsync()
+                Return responseContent
+            Else
+                Console.WriteLine("Error: " & response.StatusCode)
+                Return String.Empty
+            End If
+        End Function
 
     End Class
 
--- Views/MainWindow.xaml.vb
+++ Views/MainWindow.xaml.vb
@@ -1,4 +1,6 @@
-﻿Namespace Views
+﻿Imports Application.ViewModels
+
+Namespace Views
 
     Class MainWindow
 
@@ -7,6 +9,14 @@
             Dim email As String = txtEmail.Text
 
             MessageBox.Show($"Name: {name}{Environment.NewLine}Email: {email}", "Form Data")
+        End Sub
+
+        Private Async Sub Button_Click(sender As Object, e As RoutedEventArgs)
+            Dim viewModel As MainWindowViewModel = TryCast(DataContext, MainWindowViewModel)
+
+            Dim response As String = Await viewModel.PostDataAsync()
+
+            MessageBox.Show($"{response}")
         End Sub
 
     End Class
